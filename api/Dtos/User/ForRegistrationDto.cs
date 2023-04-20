fa009c358cb7e87380be4e24a18e26989c9d84c4
Rashmi <rashmiwijesekara1999@gmail.com>
2023-04-20 18:55:11 +0530

    Add User model & registration DTO

---
+++ api/Dtos/User/ForRegistrationDto.cs
@@ -0,0 +1,25 @@
+﻿namespace api.Dtos
+{
+    public class ForRegistrationDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string Cv { get; set; }
+        public string CoverLetter { get; set; }
+        public int LocationId { get; set; }
+
+        public ForRegistrationDto()
+        {
+            FirstName ??= "";
+            LastName ??= "";
+            Password ??= "";
+            PasswordConfirm ??= "";
+            Email ??= "";
+            Cv ??= "";
+            CoverLetter ??= "";
+        }
+    }
+}
---
+++ api/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace api.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Cv { get; set; }
+        public string CoverLetter { get; set; }
+        public int LocationId { get; set; }
+
+        public User()
+        {
+            FirstName ??= "";
+            LastName ??= "";
+            PasswordHash ??= "";
+            PasswordSalt ??= "";
+            Email ??= "";
+            Cv ??= "";
+            CoverLetter ??= "";
+        }
+    }
+}
